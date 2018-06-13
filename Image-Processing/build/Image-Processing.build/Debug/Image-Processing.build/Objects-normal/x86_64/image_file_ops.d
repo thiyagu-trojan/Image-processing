73203c66278424d87bb05babab9a918c538614c2
Thiyagu <thiyagu.1405@gmail.com>
2018-06-13 17:29:20 +0530

    First Commit

---
+++ Image-Processing/build/Image-Processing.build/Debug/Image-Processing.build/Objects-normal/x86_64/image_file_ops.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/thiyagarajanramanathan/Desktop/Image-processing/Image-Processing/Image-Processing/image_file_ops.cpp \
+  /Users/thiyagarajanramanathan/Desktop/Image-processing/Image-Processing/Image-Processing/image_file_ops.hpp
---
+++ Image-Processing/build/Image-Processing.build/Debug/Image-Processing.build/Objects-normal/x86_64/main.d
@@ -0,0 +1,4 @@
+dependencies: \
+  /Users/thiyagarajanramanathan/Desktop/Image-processing/Image-Processing/Image-Processing/main.cpp \
+  /Users/thiyagarajanramanathan/Desktop/Image-processing/Image-Processing/Image-Processing/image_file_ops.hpp \
+  /Users/thiyagarajanramanathan/Desktop/Image-processing/Image-Processing/Image-Processing/basic_image_ops.hpp
